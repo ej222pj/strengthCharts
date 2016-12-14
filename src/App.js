4fd8d30f3508a4587c39b49f9b99dabac839f7fa
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2016-12-15 00:53:06 +0100

    Added images to each exercise

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import Deadlift from './components/exercise/Deadlift';
 import Press from './components/exercise/Press';
 import PowerClean from './components/exercise/PowerClean';
 import PowerSnatch from './components/exercise/PowerSnatch';
-
+const BenchImage = '/images/Bench.png';
+const DeadliftImage = '/images/Deadlift.png';
+const SquatImage = '/images/Squat.png';
+const PressImage = '/images/Press.png';
+const CleanImage = '/images/Clean.jpg';
+const SnatchImage = '/images/Snatch.png';
 
 class App extends Component {
   constructor(props) {
@@ -44,13 +49,22 @@ class App extends Component {
                   <div className="informationPart"><h5>Advanced: </h5><p>Done the exercise for multiple years!</p></div>
                   <div className="informationPart"><h5>Elite: </h5><p>Athlete competing in the sport!</p></div>
                 </div>
-                <div style={{width: '100%'}}>
+                <div className="tables">
                   {this.state.exerciseTab === 0 ? <Bench pounds={doConvert} /> : null}
                   {this.state.exerciseTab === 1 ? <Deadlift pounds={doConvert} /> : null}
                   {this.state.exerciseTab === 2 ? <Squat pounds={doConvert} /> : null}
                   {this.state.exerciseTab === 3 ? <Press pounds={doConvert} /> : null}
                   {this.state.exerciseTab === 4 ? <PowerClean pounds={doConvert} /> : null}
                   {this.state.exerciseTab === 5 ? <PowerSnatch pounds={doConvert} /> : null}
+                </div>
+                <div className="imagesPlaceholder">
+                  {this.state.exerciseTab === 0 ? <img src={BenchImage} className='exerciseImage' alt='How to do Bench Press'/> : null}
+                  {this.state.exerciseTab === 1 ? <img src={DeadliftImage} className='exerciseImage' alt='How to do Deadlift'/> : null}
+                  {this.state.exerciseTab === 2 ? <img src={SquatImage} className='exerciseImage' alt='How to do Squat'/> : null}
+                  {this.state.exerciseTab === 3 ? <img src={PressImage} className='exerciseImage' alt='How to do Press'/> : null}
+                  {this.state.exerciseTab === 4 ? <img src={CleanImage} className='exerciseImage' alt='How to do Power Clean'/> : null}
+                  {this.state.exerciseTab === 5 ? <img src={SnatchImage} className='exerciseImage' alt='How to do Power Snatch'/> : null}
+
                 </div>
               </div>
             </div>
